8d9d7fa562ee59592dc91e2ec92bc8357d6a3547
Aditya Yadav <adityadav023>
2022-02-12 03:06:34 +0530

    jwt token created

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,27 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const CustomAPIError = require('../errors/custom-error')
+
 const login = async (req,res)=>{
-    res.status(200)
-    .send('Fake login/Register done.')
+    const {username, password} = req.body
+    //validating username and password
+    if(!username || !password){
+        throw new CustomAPIError('Please provide email and password',400)
+    }
+    //demo id, usually provide by db
+    const id = new Date().getDate()
+
+    const token = jwt.sign({id, username},
+        process.env.JWT_SECRET,
+        {
+            expiresIn: '30d'
+        }
+        )
+
+    res.status(200).json({
+        msg:"token created",
+        token
+    })
 }
 
 const dashboard = async (req,res)=>{
